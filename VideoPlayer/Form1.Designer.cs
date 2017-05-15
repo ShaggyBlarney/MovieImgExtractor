fcb93e47b1b591cb0d18518b43c26f08d3293673
ShaggyBlarney <kearney_@hotmail.com>
2017-05-15 10:29:35 -0700

    VideoPlayer

---
+++ VideoPlayer/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace VideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.VideoPanel = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.PlayPauseBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SeekTrackBar = new System.Windows.Forms.TrackBar();
+            this.TimeTextBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SeekTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VideoPanel
+            // 
+            this.VideoPanel.Location = new System.Drawing.Point(12, 44);
+            this.VideoPanel.Name = "VideoPanel";
+            this.VideoPanel.Size = new System.Drawing.Size(908, 510);
+            this.VideoPanel.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(932, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileButton});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(211, 30);
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // PlayPauseBtn
+            // 
+            this.PlayPauseBtn.Location = new System.Drawing.Point(12, 605);
+            this.PlayPauseBtn.Name = "PlayPauseBtn";
+            this.PlayPauseBtn.Size = new System.Drawing.Size(98, 23);
+            this.PlayPauseBtn.TabIndex = 2;
+            this.PlayPauseBtn.Text = "⏯";
+            this.PlayPauseBtn.UseVisualStyleBackColor = true;
+            this.PlayPauseBtn.Click += new System.EventHandler(this.PlayPauseBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(117, 605);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 3;
+            this.StopBtn.Text = "⏹";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // SeekTrackBar
+            // 
+            this.SeekTrackBar.Location = new System.Drawing.Point(217, 561);
+            this.SeekTrackBar.Name = "SeekTrackBar";
+            this.SeekTrackBar.Size = new System.Drawing.Size(564, 69);
+            this.SeekTrackBar.TabIndex = 4;
+            this.SeekTrackBar.Scroll += new System.EventHandler(this.SeekTrackBar_Scroll);
+            // 
+            // TimeTextBox
+            // 
+            this.TimeTextBox.Location = new System.Drawing.Point(787, 561);
+            this.TimeTextBox.Name = "TimeTextBox";
+            this.TimeTextBox.Size = new System.Drawing.Size(100, 26);
+            this.TimeTextBox.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 640);
+            this.Controls.Add(this.TimeTextBox);
+            this.Controls.Add(this.SeekTrackBar);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.PlayPauseBtn);
+            this.Controls.Add(this.VideoPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SeekTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel VideoPanel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileButton;
+        private System.Windows.Forms.Button PlayPauseBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar SeekTrackBar;
+        private System.Windows.Forms.TextBox TimeTextBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ VideoPlayer/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.DirectX.AudioVideoPlayback;
+
+namespace VideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Video MyVideo;
+        public int PanelWidth, PanelHeight;
+        public string PlayingPosition, Duration;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Stop();
+        }
+
+        //returns time in a string formated as hh:mm:ss
+        public string CalculateTime(double Time)
+        {
+            String mm, ss;
+            int h, m, s, T;
+
+            Time = Math.Round(Time);
+            T = Convert.ToInt32(Time);
+
+            //split time T down into hours, minutes, seconds.
+            h = (T / 3600);
+            T = T % 3600;
+            m = (T / 60);
+            s = T % 60;
+
+            //adds a '0' to single digits for minutes and seconds
+            if (m < 10)
+                mm = string.Format("0{0}", m);
+            else
+                mm = m.ToString();
+
+            if (s < 10)
+                ss = string.Format("0{0}", s);
+            else
+                ss = s.ToString();
+
+            return string.Format("{0}:{1}:{2}", h, mm, ss);
+        }
+
+        [STAThread]
+        public void StartUp()
+        {
+            // clear video of any gabage data
+            if (MyVideo != null)
+                MyVideo.Dispose();
+
+            //setup file dialog and panel width and height
+            Text = openFileDialog1.SafeFileName; //+ " - MPlayer 2.0";
+            PanelWidth = VideoPanel.Width;
+            PanelHeight = VideoPanel.Height;
+
+            //associate file dialog with video
+            MyVideo = new Video(openFileDialog1.FileName);
+            MyVideo.Owner = VideoPanel;
+            VideoPanel.Width = PanelWidth;
+            VideoPanel.Height = PanelHeight;
+
+            //setup seek track bar
+            SeekTrackBar.Minimum = Convert.ToInt32(MyVideo.CurrentPosition);
+            SeekTrackBar.Maximum = Convert.ToInt32(MyVideo.Duration);
+            SeekTrackBar.Value = 0;
+            PlayPauseBtn.Text = "\u23F8";
+
+            Duration = CalculateTime(MyVideo.Duration);
+            PlayingPosition = "00:00:00";
+            TimeTextBox.Text = PlayingPosition + " / " + Duration;
+
+            //show first frame of video
+            MyVideo.Play();
+            MyVideo.Pause();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select Video File...";
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.DefaultExt = ".mov";
+            openFileDialog1.Filter = "MOV Files|*.mov|MPG Files|*.mp4|All Files|*.*";
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Console.WriteLine(openFileDialog1.SafeFileName);
+            StartUp();
+        }
+
+        private void PlayPauseBtn_Click(object sender, EventArgs e)
+        {
+            if (MyVideo != null)
+            {
+                if (MyVideo.Playing)
+                {
+                    MyVideo.Pause();
+                    timer1.Stop();
+                    PlayPauseBtn.Text = "\u23F5";
+                }
+                else
+                {
+                    MyVideo.Play();
+                    timer1.Start();
+                    PlayPauseBtn.Text = "\u23F8";
+                }
+
+            }
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            if (MyVideo != null)
+            {
+                MyVideo.StopWhenReady();
+                timer1.Stop();
+                StartUp();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SeekTrackBar.Value = Convert.ToInt32(MyVideo.CurrentPosition);
+            PlayingPosition = CalculateTime(MyVideo.CurrentPosition);
+            TimeTextBox.Text = PlayingPosition + " / " + Duration;
+
+            if (PlayingPosition == Duration)
+            {
+                timer1.Stop();
+                StartUp();
+            }
+        }
+
+        private void SeekTrackBar_Scroll(object sender, EventArgs e)
+        {
+            if (MyVideo != null)
+            {
+                MyVideo.CurrentPosition = SeekTrackBar.Value;
+            }
+            else
+            {
+                SeekTrackBar.Value = 0;
+            }
+
+        }
+
+    }
+}
